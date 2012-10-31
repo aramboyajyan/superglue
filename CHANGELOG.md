61ac1745a4889b388eca3332d69e9f634a3b35ee
topsitemakers <support@topsitemakers.com>
2012-10-31 16:56:20 +0100

    RESTful example

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@
 
 Initial release.
 
+### October 31, 2012
+
+Added RESTful example.
+
 <hr>
 
 By: [topsitemakers.com](http://www.topsitemakers.com).
--- example.php
+++ example.php
@@ -12,10 +12,17 @@ require 'superglue.php';
 
 // Define URLs
 $urls = array(
+  
   // This will be binded to class "sample" and method "index"
   '/' => 'sample',
   // This will be binded to class "sample" and method "page"
   '/sample' => array('sample', 'page'),
+
+  // RESTful example
+  // If we do not pass an array here, superglue will check if the class has
+  // method named as the request method (GET, POST, DELETE, PUSH)
+  '/restful' => 'restful',
+
 );
 
 // Our sample class
@@ -30,5 +37,22 @@ class sample {
   }
 }
 
+// Example RESTful class
+// Each method corresponds with the request method.
+class restful {
+  function GET() {
+    print 'GET method.';
+  }
+  function POST() {
+    print 'POST method.';
+  }
+  function PUSH() {
+    print 'PUSH method.';
+  }
+  function DELETE() {
+    print 'DELETE method.';
+  }
+}
+
 // Start the app
-glue::superglue($urls);
+superglue::stick($urls);
